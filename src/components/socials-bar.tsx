56f7f9ae7481ab99acce32b6c4d90fd38bc9eff1
0xHouss <tkthoussam@gmail.com>
2025-06-25 18:42:20 +0100

    fix(socials bar): reduce sizes on mobile

--- src/components/socials-bar.tsx
+++ src/components/socials-bar.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 export default function SocialsBar() {
   return (
     <div className="flex justify-between items-center h-20 px-3 lg:px-6 py-5">
-      <p className="text-[10px] lg:text-sm font-medium text-foreground/50">
+      <p className="text-xs lg:text-sm font-medium text-foreground/50">
         “if we know we do, <br />
         if we don&apos;t we learn.”
       </p>
@@ -18,9 +18,9 @@ export default function SocialsBar() {
               key={social.name}
               href={social.href}
               target="_blank"
-              className="h-10 w-10 rounded-full bg-primary flex items-center justify-center"
+              className="h-9 w-9 lg:h-10 lg:w-10 rounded-full bg-primary flex items-center justify-center"
             >
-              <Image src={`/${social.name.toLowerCase()}-dark.svg`} width={35} height={35} alt={`${social.name} Icon`} className="w-8 h-8 text-background" />
+              <Image src={`/${social.name.toLowerCase()}-dark.svg`} width={35} height={35} alt={`${social.name} Icon`} className="w-7 h-7 lg:w-8 lg:h-8 text-background" />
               <span className="sr-only">Email</span>
             </Link>
           ))}
@@ -29,10 +29,10 @@ export default function SocialsBar() {
         <div className="flex rounded-full border-2 border-accent p-1">
           <Link
             href="mailto:thoussam.pro@gmail.com"
-            className="h-10 w-10 rounded-full bg-primary flex items-center justify-center"
+            className="h-9 w-9 lg:h-10 lg:w-10 rounded-full bg-primary flex items-center justify-center"
             target="_blank"
           >
-            <MailIcon className="w-6 text-background" />
+            <MailIcon className="w-6 h-6 text-background" />
             <span className="sr-only">Email</span>
           </Link>
           <p className="p-2 font-medium hidden lg:block">thoussam.pro@gmail.com</p>
