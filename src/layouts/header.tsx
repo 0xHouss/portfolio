3d1d060ce92a0f66ca5a8f2c6f8725304415e906
0xHouss <tkthoussam@gmail.com>
2025-06-25 18:47:37 +0100

    feat(header): make navbar sticky, close menu on nav
    fix(header): improve styling for mobile version

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
   const [open, setOpen] = useState(false)
 
   return (
-    <header className='h-20 relative px-6 py-5 flex justify-between items-center border-b-2 border-accent bg-background'>
+    <header className='h-20 sticky top-0 px-6 py-5 flex justify-between items-center border-b-2 border-accent bg-background z-50'>
       <h1 className='font-bold'>0xHouss.</h1>
 
       <nav className='hidden lg:flex absolute left-1/2 -translate-x-1/2 gap-2'>
@@ -20,25 +20,33 @@ export default function Header() {
 
       <Link
         href="#contact"
-        className="liquid-button bg-background hidden lg:flex items-center gap-2 px-3 py-2 rounded-full border-2 border-accent hover:border-primary"
+        className="bg-background hidden lg:flex items-center p-1 rounded-full border-2 border-accent hover:border-primary hover:text-primary transition-colors duration-300"
       >
-        <p className="font-medium">Get in touch</p>
-        <MoveRightIcon className="w-6 h-6" />
+        <p className="font-medium p-2">Get in touch</p>
+        <MoveRightIcon className="w-6 h-6 mr-2" />
       </Link>
 
       <div className='flex lg:hidden'>
-        <Hamburger onToggle={setOpen} />
+        <Hamburger onToggle={setOpen} toggled={open} />
       </div>
 
-      <div className={cn('hidden fixed flex-col left-0 shadow-4xl right-0 top-20 p-4 bg-background border-b border-accent transition-opacity duration-300 w-screen', {
+      <div className={cn('hidden absolute flex-col left-0 shadow-4xl right-0 gap-2 top-20 p-4 bg-background border-b border-accent transition-opacity duration-300 w-screen', {
         "flex": open,
       })}>
         {sections.map((section) =>
-          <Link key={section.name} href={section.href} className='px-4 py-6 font-medium hover:text-primary transition-colors duration-300'>{section.name}</Link>
+          <Link
+            key={section.name}
+            href={section.href}
+            className='p-4 rounded-md border border-accent font-medium hover:border-primary hover:text-primary transition-colors duration-300'
+            onClick={() => setOpen(false)}
+          >
+            {section.name}
+          </Link>
         )}
         <Link
           href="#contact"
-          className="liquid-button bg-background flex items-center gap-2 px-4 py-6 justify-between rounded-md border border-accent hover:border-primary"
+          onClick={() => setOpen(false)}
+          className="flex items-center p-4 justify-between rounded-md border border-accent font-medium hover:border-primary hover:text-primary transition-colors duration-300"
         >
           <p className="font-medium">Get in touch</p>
           <MoveRightIcon className="w-6 h-6" />
